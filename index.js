1dbf2cea20e9008064e157c33fb8fd34fd037297
RuivX <71990761+RuivX@users.noreply.github.com>
2020-10-02 22:53:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,24 +15,49 @@ fs.readdir("./comandos/", (err, files) => {
     });
 })
 
+bot.on("guildMemberAdd", (member) => {
+
+    let guild = bot.guilds.cache.get("ID DO SERVIDOR QUE VOCÊ QUER RECEBER A MENSAGEM DE BOAS-VINDAS");
+    let channel = bot.channels.cache.get("CANAL QUE VOCÊ QUER RECEBER A MENSAGEM DE BOAS VINDAS");
+    let emoji = member.guild.emojis.cache.find(emoji => emoji.name === 'nome do emoji que você quer adicionar (opcional)');
+
+if(guild != member.guild) {
+    return console.log('Sai daqui rapaz')
+} else {
+
+    let embed = new Discord.MessageEmbed()
+    .setColor("RED")
+    .setAuthor(member.user.tag, member.user.displayAvatarURL())
+    .setTitle(`${emoji} SEJA BEM-VINDO(A) ${emoji}`)
+    .setImage('Coloque a imagem que você quiser')
+    .setDescription(`${member.user}, seja muito bem-vindo(a) ao servidor ${guild.name}! Atualmente estamos com ${member.guild.memberCount} membros. Por Favor, leia as regras do servidor, para você não cometer vacilos e ser retirado do servidor ou temporáriamente, ou permanentemente!`)
+    .setThumbnail(member.user.displayAvatarURL({ dynamic: true, format: "png", size: 1024}))
+    .setFooter('ID do usuário: ' + member.user.id)
+    .setTimestamp();
+
+    channel.send(embed)
+    member.send(`Aqui será a mensagem que vai ser enviada para a DM do usuário que entrar no servidor`)
+}
+});
+
 bot.on('ready', () => {
     console.log(`Bot foi iniciado com sucesso`);
     let activities = [
-        `Harry Potter`,
-        `The Last of Us 2`,
-        `DMenor11`,
-        `Deni`,
-        `RUIVO FF`,
+        `O status que você quiser`,
+        `O status que você quiser`,
+        `O status que você quiser`,
+        `O status que você quiser`,
+        `O status que você quiser`,
         `Utilize ${config.prefix}help para aparecer os comandos do bot`,
-        `${bot.guilds.cache.size} servidores.`,
-        `${bot.channels.cache.size} canais.`,
-        `${bot.users.cache.size} usuários.`,
+        `${bot.guilds.cache.size} servidores.`, //AQUI É O TANTO DE SERVIDORES QUE SEU BOT ESTÁ ATUALMENTE
+        `${bot.channels.cache.size} canais.`, //AQUI É O TANTO DE CANAIS QUE TEM NOS SERVIDORES QUE SEU BOT ESTÁ
+        `${bot.users.cache.size} usuários.`, //AQUI É O TANTO DE USUÁRIOS QUE TEM NOS SERVIDORES QUE SEU BOT ESTÁ
         `Você dormir.`
     ],
     i = 0;
     setInterval(() => bot.user.setActivity(`${activities[i++ % activities.length]}`, {
-        type: "WATCHING"
-    }), 10000); //WATCHING, LISTENING, PLAYING, STREAMING
+        type: " " //WATCHING, LISTENING, PLAYING, STREAMING
+    }), 10000); //TEMPO QUE O STATUS DO BOT IRÁ MUDAR, O MEU É 10 SEGUNDOS.
     setStatus("online") //idle, dnd, online, invisible
     .catch(console.log);
 }); 
