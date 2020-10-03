81f40c04ee8750039f4e4976b61b54f86cc71559
RuivX <71990761+RuivX@users.noreply.github.com>
2020-10-02 21:34:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,31 +15,6 @@ fs.readdir("./comandos/", (err, files) => {
     });
 })
 
-bot.on("guildMemberAdd", (member) => {
-
-    let guild = bot.guilds.cache.get("703809493279965215");
-    let channel = bot.channels.cache.get("722500911640870933");
-    let emoji = member.guild.emojis.cache.find(emoji => emoji.name === 'leals');
-
-if(guild != member.guild) {
-    return console.log('Sai daqui rapaz')
-} else {
-
-    let embed = new Discord.MessageEmbed()
-    .setColor("RED")
-    .setAuthor(member.user.tag, member.user.displayAvatarURL())
-    .setTitle(`${emoji} SEJA BEM-VINDO(A) ${emoji}`)
-    .setImage('https://cdn.discordapp.com/attachments/735287053398310943/739230564250288219/IMG_20200528_130801_916.jpg')
-    .setDescription(`${member.user}, seja muito bem-vindo(a) ao servidor ${guild.name}! Atualmente estamos com ${member.guild.memberCount} membros. Por Favor, leia as regras do servidor, para você não cometer vacilos e ser retirado do servidor ou temporáriamente, ou permanentemente!`)
-    .setThumbnail(member.user.displayAvatarURL({ dynamic: true, format: "png", size: 1024}))
-    .setFooter('ID do usuário: ' + member.user.id)
-    .setTimestamp();
-
-    channel.send(embed)
-    member.send(`Olá, seja muito bem-vindo ao servidor L E A L S, eu sou o Core, o bot criado pelo administrador do servidor, RUIVO. Espero que você se divirta aqui dentro, até mais.`)
-}
-});
-
 bot.on('ready', () => {
     console.log(`Bot foi iniciado com sucesso`);
     let activities = [
