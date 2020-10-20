5f633e5ba14275ee6014efb29b92d2ea55ba4d75
RuivX <71990761+RuivX@users.noreply.github.com>
2020-10-20 14:53:10 -0300

    Delete perfeito.js

--- comandos/perfeito.js
+++
@@ -1,9 +0,0 @@
-const Discord = require ("discord.js");
-
-exports.run = async (bot, message, args) => {
-    await message.channel.send(`:writing_hand: | 𝖯 𝖤 𝖱 𝖥 𝖤 𝖨 𝖳 𝖮`)
-}
-
-exports.help = {
-    name: 'perfeito'
-}
