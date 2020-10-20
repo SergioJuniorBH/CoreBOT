7e2bcd3f01e07ba40811c6e7764684b0c38bd491
RuivX <71990761+RuivX@users.noreply.github.com>
2020-10-20 14:55:03 -0300

    Delete tio.js

--- comandos/tio.js
+++
@@ -1,9 +0,0 @@
-const Discord = require ("discord.js");
-
-exports.run = async (bot, message, args) => {
-    await message.channel.send("É pavê ou... cê achou que eu ia fazer essa piada né?!")
-}
-
-exports.help = {
-    name: 'tio'
-}
