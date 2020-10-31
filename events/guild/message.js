6c1ea2357c29b219a4a43e5a109ebcb1720e9177
RuivX <71990761+RuivX@users.noreply.github.com>
2020-10-30 23:24:08 -0300

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -5,14 +5,16 @@ module.exports = async (bot, message) => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
     if(message.content == `<${bot.user.id}>` || message.content == `<@!${bot.user.id}>`) {
-        return message.channel.send(`:man_raising_hand: | Opa, me chamou?\nSe sim eu sou o Core, meu prefixo nesse servidor é \`c!\`\nPara qualquer dúvida sobre mim, entre neste servidor: https://discord.gg/5vcRmQK \nAté mais!`)
+        return message.channel.send(`:man_raising_hand: | Opa, me chamou?\nSe sim eu sou o Core, meu prefixo nesse servidor é \`c!\`\nPara qualquer dúvida sobre mim, entre neste servidor: https://discord.gg/NkzUCsM \nAté mais!`)
     };
 
     if(message.content === "Oi Core") {
-        message.channel.send(`:grin: | Oi, tudo bom?`)
+        message.channel.send(`:grin: | Eae, tudo bom?`)
     };
 
-    const args = message.content.trim().slice(prefix.length).split(/ +/g);
+    if(!message.content.startsWith(prefix)) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
     let command;
 
