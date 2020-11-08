066035a044f90c83da2948b9fb45bc08e5dac15f
RuivX <71990761+RuivX@users.noreply.github.com>
2020-11-08 12:58:34 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const Discord = require('discord.js');
 const fs = require('fs')
 const config = require('./config.json')
 const search = require('yt-search');
+const db = require('quick.db')
 const bot = new Discord.Client({
     disableEveryone: true
 });
-const prefix = (config.prefix);
+const prefix = (config.default_prefix)
+const { addexp } = require('./handlers/xp');
 
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
