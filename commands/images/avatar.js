2e3f54ab8e5387ee2ac48a9841662511b0d0a87e
RuivX <71990761+RuivX@users.noreply.github.com>
2020-11-08 14:29:48 -0300

    Update avatar.js

--- commands/images/avatar.js
+++ commands/images/avatar.js
@@ -5,20 +5,21 @@ module.exports = {
     category: 'images',
     description: 'Take the avatar of some member',
     run: async (bot, message, args) => {
-        let embed = new Discord.MessageEmbed()
         if(!message.mentions.users.first()) {
-            embed.setTitle("🖼️ **SEU AVATAR**")
-            embed.setDescription(`[Clique aqui](${message.author.displayAvatarURL({ dynamic: true, format: "png", size: 1024 })}) para baixar o avatar`)
-            embed.setImage(message.author.displayAvatarURL({ dynamic: true, format: "png", size: 1024}))
-            embed.setColor("RANDOM")
-            return message.channel.send(embed)
+            const embed = new Discord.MessageEmbed()
+            .setTitle("🖼️ | SEU AVATAR")
+            .setDescription(`[Clique aqui](${message.author.displayAvatarURL({ dynamic: true, format: "png", size: 1024 })}) para baixar o avatar`)
+            .setImage(message.author.displayAvatarURL({ dynamic: true, format: "png", size: 1024}))
+            .setColor("RANDOM")
+            message.channel.send(embed)
         } else {
+            const embed2 = new Discord.MessageEmbed()
             let user = message.mentions.users.first()
-            embed.setTitle(`**🖼️ AVATAR DE ${user.tag}**`)
-            embed.setDescription(`[Clique aqui](${user.displayAvatarURL({ dynamic: true, format: "png", size: 1024 })}) para baixar o avatar`)
-            embed.setImage(user.displayAvatarURL({ dynamic: true, format: "png", size: 1024}))
-            embed.setColor("RANDOM")
-            return message.channel.send(embed)
+            .setTitle(`**🖼️ AVATAR DE ${user.tag}**`)
+            .setDescription(`[Clique aqui](${user.displayAvatarURL({ dynamic: true, format: "png", size: 1024 })}) para baixar o avatar`)
+            .setImage(user.displayAvatarURL({ dynamic: true, format: "png", size: 1024}))
+            .setColor("RANDOM")
+            message.channel.send(embed2)
         }
     }
 }
