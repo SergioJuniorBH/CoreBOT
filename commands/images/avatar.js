a23fa23b28db69e976af3ceada0f92d94f61a9ae
RuivX <71990761+RuivX@users.noreply.github.com>
2020-11-08 14:08:22 -0300

    Update avatar.js

--- commands/images/avatar.js
+++ commands/images/avatar.js
@@ -7,14 +7,14 @@ module.exports = {
     run: async (bot, message, args) => {
         let embed = new Discord.MessageEmbed()
         if(!message.mentions.users.first()) {
-            embed.setTitle("🖼️ | **SEU AVATAR**")
+            embed.setTitle("🖼️ **SEU AVATAR**")
             .setDescription(`[Clique aqui](${message.author.displayAvatarURL({ dynamic: true, format: "png", size: 1024 })}) para baixar o avatar`)
             embed.setImage(message.author.displayAvatarURL({ dynamic: true, format: "png", size: 1024}))
             embed.setColor("RANDOM")
             return message.channel.send(embed)
         } else {
             let user = message.mentions.users.first()
-            embed.setTitle(`**🖼️ | AVATAR DE ${user.tag}**`)
+            embed.setTitle(`**🖼️ AVATAR DE ${user.tag}**`)
             .setDescription(`[Clique aqui](${user.displayAvatarURL({ dynamic: true, format: "png", size: 1024 })}) para baixar o avatar`)
             embed.setImage(user.displayAvatarURL({ dynamic: true, format: "png", size: 1024}))
             embed.setColor("RANDOM")
