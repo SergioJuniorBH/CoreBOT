e11cf18586e88ce9767fcc220e7466e21bd1f235
RuivX <71990761+RuivX@users.noreply.github.com>
2020-11-10 23:52:00 -0300

    Update userinfo.js

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -15,55 +15,28 @@ module.exports = {
             const embed = new Discord.MessageEmbed()
             .setAuthor(`Informações sobre ${message.author.username}`)
             .setThumbnail(message.author.displayAvatarURL({ dynamic: true, format: "png", size: 1024 }))
-            .addFields({
-                name: 'Tag do usuário',
-                value: message.author.tag
-            }, {
-                name: 'Nickname',
-                value: message.author.nickname || 'Nenhum'
-            }, {
-                name: 'Criou sua conta do Discord em',
-                value: moment(message.author.createdTimestamp).format('DD/MM/YYYY')
-            }, {
-                name: 'ID',
-                value: message.author.id
-            }, {
-                name: 'Status',
-                value: message.author.presence.status
-            }, {
-                name: 'Jogo que está jogando agora',
-                value: message.author.presence.activities
-            })
+            .addField('Tag do Usuário', message.author.tag, true)
+            .addField('Nickname', message.author.nickname || 'Nenhum', true)
+            .addField('Criou sua Conta do Discord em', moment(message.author.createdTimestamp).format('DD/MM/YYYY'))
+            .addField('ID', message.author.id, true)
+            .addField('Status', message.author.presence.status, true)
+            .addField('Está Jogando', message.author.presence.activities)
             .setTimestamp()
 
             message.channel.send(embed)
 
         } else {
-            const embed2 = new Discord.MessageEmbed()
-            .setAuthor(`Informações sobre ${user}`)
-            .setThumbnail(user.displayAvatarURL({ dynamic: true, format: "png", size: 1024 }))
-            .addFields({
-                name: 'Tag do usuário',
-                value: user.tag
-            }, {
-                name: 'Nickname',
-                value: user.nickname || 'Nenhum'
-            }, {
-                    name: 'Criou sua conta do Discord em',
-                    value: moment(message.author.createdTimestamp).format('DD/MM/YYYY')
-            }, {
-                name: 'ID',
-                value: user.id
-            }, {
-                name: 'Status',
-                value: user.presence.status
-            }, {
-                name: 'Jogo que está jogando agora',
-                value: user.presence.activities
-            })
-            .setTimestamp()
+            const embed = new Discord.MessageEmbed()
+            embed.setAuthor(`Informações sobre ${user}`)
+            embed.setThumbnail(user.displayAvatarURL({ dynamic: true, format: "png", size: 1024 }))
+            embed.addField('Tag do Usuário', user.tag, true)
+            embed.addField('Nickname', user.nickname || 'Nenhum', true)
+            embed.addField('Criou sua Conta do Discord em', moment(user.createdTimestamp).format('DD/MM/YYYY'))
+            embed.addField('ID', user.id, true)
+            embed.addField('Status', user.presence.status, true)
+            embed.setTimestamp()
 
-            message.channel.send(embed2)
+            message.channel.send(embed)
         }
     }
 }
