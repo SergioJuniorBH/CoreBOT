be7ad3235145d3e888b01635f1521cc4efa699af
RuivX <71990761+RuivX@users.noreply.github.com>
2020-11-18 01:35:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const fs = require('fs')
 const config = require('./config.json')
-const search = require('yt-search');
-const db = require('quick.db')
 const bot = new Discord.Client({
     disableEveryone: true
 });
@@ -11,7 +9,6 @@ const prefix = (config.default_prefix)
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 bot.categories = fs.readdirSync("./commands/");
-bot.queues = new Map();
 
 ["command"].forEach(handler => {
     require(`./handlers/${handler}`)(bot);
