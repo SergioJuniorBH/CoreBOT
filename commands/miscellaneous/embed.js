a02526a56d5438145db503e6df98a860d3e3419b
Artur <71990761+RuivX@users.noreply.github.com>
2020-11-18 20:55:51 -0300

    Add files via upload

---
+++ commands/miscellaneous/embed.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'embed',
+    category: 'miscellaneous',
+    description: 'Make a embed message',
+    run: async (bot, message, args) => {
+        let argument = args.join(" ");
+        if(!argument) {
+            return message.channel.send('<:not_stonks:776890257198678017> | Você não especificou o conteúdo do embed')
+        }
+
+        const persoEmbed = new Discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle('EMBED PERSONALIZADO')
+        .setAuthor(`Banner feito por ${message.author.username}`)
+        .setDescription(argument)
+        .setTimestamp()
+
+        message.channel.send(persoEmbed)
+    }
+}
